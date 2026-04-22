fbfa0b75afedb353fb2a382e8ead5354da9ced3a
Erick Silva <donizttierick@gmail.com>
2026-04-22 11:23:47 -0300

    Ajuste, Iniciando o carregamento dos dados

---
+++ frontend/src/global/ts/indentidadeUser.ts
@@ -0,0 +1,10 @@
+/* Objetivo: Aqui eu vou pegar o token JWT do localstorage e verificar se é realmente o usuario  se for eu carrego os dados se não da erro*/
+
+// Saidas aonde vai ir o nome
+const saidaNome = document.querySelector('.nome-usuario') as HTMLParagraphElement;
+
+// Verificando se existe
+if (saidaNome) {
+
+    
+}
